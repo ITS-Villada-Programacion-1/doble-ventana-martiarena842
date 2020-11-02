17d0dea11a5db57f9064e3b1e29bb9d3994d8521
martiarena842 <71388907+martiarena842@users.noreply.github.com>
2020-11-02 11:53:50 -0300

    Add files via upload

--- receta.py
+++ receta.py
@@ -1,7 +1,20 @@
-import sys
+import sys, os
 from PySide2.QtCore import Slot
 from PySide2.QtWidgets import QApplication, QMainWindow
-from principal import Ui_Buscardor
+from principal_ui import Ui_Buscardor
+from resultado_ui import Ui_Resultado
+
+
+class Resultado(QMainWindow):
+    def __init__(self):
+        super(Resultado, self).__init__()
+        self.ui = Ui_Resultado()
+        self.ui.setupUi(self)
+
+    @Slot()
+    def cerrar(self):
+        self.close()
+
 
 class Principal(QMainWindow):
     def __init__(self):
@@ -11,8 +24,22 @@ class Principal(QMainWindow):
         self.opened_file = None
 
     @Slot()
-    def func(self):
-        pass
+    def buscar(self):
+        self.nombre = self.ui.lblNombre.text() #Lee la caja de texto
+        self.user = os.popen("whoami").read() #Obtiene el usuario
+        self.user = self.user.rsplit() #Elimina el salto de carro
+        self.deruta = "/home/"+self.user[0] #Arma la ruta
+        os.chdir(self.deruta) #Configura donde se ejecuta el comando
+        self.resultado = os.popen("find -not -path '*/\.*' | grep '" + self.nombre + "'").read() #Busca los archivos que coinciden
+        print(self.resultado)
+        self.ventanita = Resultado() #Se crea un objeto que es la ventana nueva
+        self.ventanita.ui.txtResultado.setText(self.resultado) #Le pasa el resultado a la caja de texto
+        self.ventanita.show() #Configura donde se va a efectuar el comando
+
+    @Slot()
+    def borrar(self):
+        self.ui.lblNombre.setText("")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
